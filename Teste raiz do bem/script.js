57888eeb97aafb9176a125e90289090ccd019d50
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2024-11-13 10:23:03 -0300

    Adicionado

--- Teste raiz do bem/script.js
+++ Teste raiz do bem/script.js
@@ -1,27 +1,31 @@
-const swiper = new Swiper('.swipwer', {
-    direction:'vertical',
+const swiper = new Swiper('.card-wrapper', {
     loop: true,
-    spaceBetween: 3,
-
+    spaceBetween: 30, // Corrected the typo here
+    
+    // Pagination bullet
     pagination: {
-        el: '.swipwr-pagination',
-        clickable: true,
-        dynamicBullets: true
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
-    navegation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-
+  
+    // Responsive breakpoints
     breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3
-        },
+      0: {
+        slidesPerView: 1, // Show 1 slide at a time for small screens
+      },
+      768: {
+        slidesPerView: 2, // Show 2 slides at a time for tablets or larger screens
+      },
+      1024: {
+        slidesPerView: 3, // Show 3 slides at a time for desktop or larger screens
+      },
     }
-})
+  });
+  
